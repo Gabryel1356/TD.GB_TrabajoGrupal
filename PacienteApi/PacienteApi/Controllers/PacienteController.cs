bd7c0163f608a84e77e4da0ba698343bcfbaaa82
Gabryel1356 <117868063+Gabryel1356@users.noreply.github.com>
2023-01-22 16:37:41 -0500

    .

---
+++ PacienteApi/PacienteApi/Controllers/PacienteController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Threading.Tasks;
+using PacienteApi.Context;
+using PacienteApi.Models;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PacienteApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacienteController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public PacienteController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+
+
+
+
+        // GET: api/<PacienteController>
+        [HttpGet]
+        public IEnumerable<Paciente> Get()
+        {
+            return context.Pacientes.ToList();
+        }
+
+
+        // GET api/<PacienteController>/5
+        [HttpGet("{id}")]
+        public Paciente Get(int id)
+        {
+            var paciente = context.Pacientes.FirstOrDefault(p => p.idPac == id);
+            return paciente;
+        }
+
+        // POST api/<PacienteController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PacienteController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PacienteController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
